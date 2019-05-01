deac8df09d0e770bd7d81c95fa583710eff20f59
Ivan Golikov <igolikov@octoberry.ru>
2019-05-01 18:10:16 +0200

    Initial commit

---
+++ src/main/java/jp/ivan/swadeshness/SwadeshnessApplication.java
@@ -0,0 +1,13 @@
+package jp.ivan.swadeshness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwadeshnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwadeshnessApplication.class, args);
+	}
+
+}
