f4790d2bf42019bb7f8701c3b331262d9a5d3bd7
Ivan Golikov <igolikov@octoberry.ru>
2019-06-02 19:00:56 +0300

    Refactored to ResponseEntity

--- src/main/java/jp/ivan/swadeshness/controller/LanguagesUpdateController.java
+++ src/main/java/jp/ivan/swadeshness/controller/LanguagesUpdateController.java
@@ -5,10 +5,11 @@ import jp.ivan.swadeshness.service.GitServiceImpl;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
@@ -25,11 +26,11 @@ public class LanguagesUpdateController {
     @Autowired
     private Environment env;
 
-    @RequestMapping(value = "/words/{word}", method = RequestMethod.PUT)
-    public String index(@PathVariable String word) throws GitAPIException, IOException, ExecutionException, InterruptedException {
+    @PutMapping(value = "/words/{word}")
+    ResponseEntity<String> index(@PathVariable String word) throws GitAPIException, IOException, ExecutionException, InterruptedException {
         gitService = getGitService();
         gitService.pushAll(word);
-        return "Words list is updated with word" + word;
+        return new ResponseEntity<>("Words list is updated with word" + word, HttpStatus.OK);
     }
 
     private ExecutorService getTaskExecutor() {
