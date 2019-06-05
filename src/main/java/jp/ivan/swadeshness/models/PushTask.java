67899df0100e3c08ecab682bbe5be278a18674a3
Ivan Golikov <igolikov@octoberry.ru>
2019-06-05 23:13:13 +0300

    Refactored PushTask a bit

---
+++ src/main/java/jp/ivan/swadeshness/models/PushTask.java
@@ -0,0 +1,58 @@
+package jp.ivan.swadeshness.models;
+
+import jp.ivan.swadeshness.service.GitServiceImpl;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PushCommand;
+import org.eclipse.jgit.api.TransportConfigCallback;
+import org.eclipse.jgit.transport.SshTransport;
+import org.eclipse.jgit.transport.Transport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.concurrent.Callable;
+
+public class PushTask implements Callable<Void> {
+    public static final String WORDS_REPO_DIR = "words-list";
+    private static final String WORDS_FILE = "/words";
+
+
+    private Logger logger = LoggerFactory.getLogger(PushTask.class);
+
+    private GitServiceImpl gitService;
+    private String message;
+
+    public PushTask(GitServiceImpl gitService) {
+        this.gitService = gitService;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public Void call() throws Exception {
+        Git git = gitService.getRepo();
+        BufferedWriter out = new BufferedWriter(
+                new FileWriter(WORDS_REPO_DIR + WORDS_FILE, true));
+        out.newLine();
+        out.write(message);
+        out.close();
+        git.commit().setAll(true).setMessage("Updated words list with word " + message).call();
+        logger.debug("Commit made");
+
+        PushCommand pushCommand = git
+                .push()
+                .setTransportConfigCallback( new TransportConfigCallback() {
+                    @Override
+                    public void configure(Transport transport) {
+                        SshTransport sshTransport = ( SshTransport )transport;
+                        sshTransport.setSshSessionFactory( gitService.getSessionFactory() );
+                    }
+                });
+
+        pushCommand.call();
+        return null;
+    }
+}
--- src/main/java/jp/ivan/swadeshness/service/GitService.java
+++ src/main/java/jp/ivan/swadeshness/service/GitService.java
@@ -2,6 +2,7 @@ package jp.ivan.swadeshness.service;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.transport.SshSessionFactory;
 import org.springframework.core.env.Environment;
 
 import java.io.IOException;
@@ -11,5 +12,7 @@ import java.util.concurrent.ExecutorService;
 public interface GitService {
     void setEnv(Environment env);
     void setExecutor(ExecutorService executor);
+    Git getRepo() throws IOException, GitAPIException;
+    SshSessionFactory getSessionFactory();
     void pushAll(String message) throws IOException, GitAPIException, ExecutionException, InterruptedException;
 }
